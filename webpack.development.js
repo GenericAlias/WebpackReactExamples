8b2f08436afddd824e799ab6bb7d979b952b87e3
GenericAlias <danialc@uw.edu>
2015-07-09 11:57:27 -0700

    Added webpack.development.js

---
+++ webpack.development.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+var ROOT_PATH = path.resolve(__dirname);
+
+module.exports = {
+    entry: [
+      'webpack/hot/dev-server',
+      path.resolve(ROOT_PATH, 'app/main.js')
+    ],
+    output: {
+      path: path.resolve(ROOT_PATH, 'build'),
+      filename: 'bundle.js',
+    }
+};
